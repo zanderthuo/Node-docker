3cfda759d688c028c580c7fdb9f80b21d4c2d944
zanderthuo <thuozander@gmail.com>
2023-01-04 17:50:49 +0300

    Added new change

--- index.js
+++ index.js
@@ -2,25 +2,31 @@ const express = require('express');
 const mongoose = require("mongoose");
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
 
+const postRouter = require('./routes/postRoutes')
+
 const app = express();
 
-const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+// const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+const mongoURL = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@dockernodeexpress.b4nkrou.mongodb.net/?retryWrites=true&w=majority`
 
 const connectWithRetry = () => {
-  mongoose
-  .connect(mongoURL)
-  .then(() => console.log('successfully connected to DB'))
-  .catch((e) => {
-    console.log(e)
-    setTimeout(connectWithRetry, 5000)
-  });
+    mongoose
+        .connect(mongoURL)
+        .then(() => console.log('successfully connected to DB'))
+        .catch((e) => {
+            console.log(e)
+            setTimeout(connectWithRetry, 5000)
+        });
 }
 
 connectWithRetry();
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('<h2>Hi, I am Testing Api!pp</h2>')
+app.get('/main', (req, res) => {
+    res.send('<h2>Hi, I am Testing Api!pp</h2>')
 });
+
+app.use('/', postRouter)
 
 const port = process.env.PORT || 5000;
 
